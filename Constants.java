0dcb7a3f9a2ca37fff022a4f9a4f2a167fee3fed
AkOnGitHub <91102463+AkOnGitHub@users.noreply.github.com>
2022-02-07 19:32:55 -0800

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+public class Constants {
+    public static final int idL1 = 0;
+    public static final int idL2 = 1;
+    public static final int idR1 = 2;
+    public static final int idR2 = 3;
+
+    public static final int joystickLId = 1;
+    public static final int joystickRId = 2;
+}
